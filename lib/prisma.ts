a756dd977a734489aebbe8f6a0fdee57b795e7af
ehsana00 <a12346516@unet.univie.ac.at>
2026-07-09 20:22:16 +0200

    Fix Prisma Client generation output and add rhel binary target for Vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const golabalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
 
